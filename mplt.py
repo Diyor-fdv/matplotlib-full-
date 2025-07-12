70ef4d46988be7c24bdd9042b09bd8f8d774dc13
Diyorbek <126152910+Diyor-fdv@users.noreply.github.com>
2025-07-12 14:29:32 +0500

    Add files via upload

---
+++ mplt.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+x=[1,2,3]
+y=[2,4,6]
+plt.plot(x,y)
+plt.title('first graph')
+plt.xlabel('time')
+plt.ylabel('zone')
+plt.show()
